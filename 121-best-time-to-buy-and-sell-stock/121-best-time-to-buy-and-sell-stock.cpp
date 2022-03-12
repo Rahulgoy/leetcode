cf79d3b3f215791304856e370cc6bd453de82c51
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-03-12 19:07:20 +0530

    Time: 161 ms (65.40%), Space: 93.3 MB (53.88%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit=0;
-        int maxi=prices[0],mini=prices[0];
-        for(int i=0;i<prices.size();i++){
-            if(prices[i]<mini){
-                mini=maxi=prices[i];
-            }else if(prices[i]>mini and prices[i]>maxi){
-                maxi=prices[i];
-            }
-            profit=max(profit, maxi-mini);
+        // int profit=0;
+        // int maxi=prices[0],mini=prices[0];
+        // for(int i=0;i<prices.size();i++){
+        //     if(prices[i]<mini){
+        //         mini=maxi=prices[i];
+        //     }else if(prices[i]>mini and prices[i]>maxi){
+        //         maxi=prices[i];
+        //     }
+        //     profit=max(profit, maxi-mini);
+        // }
+        // return profit;
+        
+        
+        //   Iterating from Backwards      
+        int n = prices.size();
+        int mx = prices[n - 1];
+        int mx_profit = 0, price = 0;
+        // in a reversive order, make sure max is behind now
+        for (int i = n - 1; i >= 0; i--){
+            price = prices[i];
+            mx = mx > price? mx:price;
+            mx_profit = mx - price > mx_profit? mx - price : mx_profit;
         }
-        return profit;
+        return mx_profit;
     }
 };
