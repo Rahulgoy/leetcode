0f4accc03e2dd5cf663133fde2698987ae27b68f
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-04-21 09:49:55 +0530

    Time: 161 ms (43.89%), Space: 56.9 MB (30.06%) - LeetHub

--- 705-design-hashset/705-design-hashset.cpp
+++ 705-design-hashset/705-design-hashset.cpp
@@ -1,21 +1,31 @@
 class MyHashSet {
 public:
-    unordered_map<int,int> hash;
+    // unordered_map<int,int> hash;
+    
+    int size = 1000000;
+    int Node[1000000];
+    int hash(int key) {
+        return key%size;
+    }
     MyHashSet() {
         
     }
     
     void add(int key) {
-        hash[key]=1;
+        // hash[key]=1;
+        Node[hash(key)]=1;
     }
     
     void remove(int key) {
-        hash[key]=0;
+        // hash[key]=0;
+        Node[hash(key)]=0;
     }
     
     bool contains(int key) {
-        if(hash[key]) return true;
-        else return false;
+        // if(hash[key]) return true;
+        // else return false;
+        
+        return Node[hash(key)]==1;
     }
 };
 
