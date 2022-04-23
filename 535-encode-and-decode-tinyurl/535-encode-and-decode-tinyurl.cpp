62e3b8c8143070d8a82ef6a212a327dd3843d7f6
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-04-23 12:29:02 +0530

    Time: 4 ms (70.07%), Space: 7 MB (45.21%) - LeetHub

--- 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
+++ 535-encode-and-decode-tinyurl/535-encode-and-decode-tinyurl.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
-    unordered_map<string,string> mp;
+    map<string,string> mp;
     // Encodes a URL to a shortened URL.
     
-    string randomGen(){
-        int len = 6;
-        static const char alphanum[] = "0123456789" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz";
-        string temp;
-        // temp.reverse(len);
-        for(int i=0;i<len;i++){
-            temp+=alphanum[rand()%(sizeof(alphanum)-1)];
-        }
-        return temp;
-    }
+    // string randomGen(){
+    //     int len = 6;
+    //     static const char alphanum[] = "0123456789" "ABCDEFGHIJKLMNOPQRSTUVWXYZ" "abcdefghijklmnopqrstuvwxyz";
+    //     string temp;
+    //     // temp.reverse(len);
+    //     for(int i=0;i<len;i++){
+    //         temp+=alphanum[rand()%(sizeof(alphanum)-1)];
+    //     }
+    //     return temp;
+    // }
     
+    int n=0;
     string encode(string longUrl) {
         string url="http://tinyurl.com/";
-        url+=randomGen();
-        
+        // url+=randomGen();
+        url+=to_string(n);
+        n++;
         mp[url]=longUrl;
         return url;
     }
