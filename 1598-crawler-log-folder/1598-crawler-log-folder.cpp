c3a0aac439047085ba9c8b4767f560a131c55542
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-05-01 12:15:23 +0530

    Time: 12 ms (22.65%), Space: 10.8 MB (24.59%) - LeetHub

--- 1598-crawler-log-folder/1598-crawler-log-folder.cpp
+++ 1598-crawler-log-folder/1598-crawler-log-folder.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     int minOperations(vector<string>& logs) {
-        int n = logs.size();
-        vector<string> res;
-        for(int i=0;i<n;i++){
-            if(logs[i]=="./");
-            else if(logs[i]=="../" and res.size()>=1) res.pop_back();
-            else if(logs[i]=="../");
-            else res.push_back(logs[i]);
-        }
+//         int n = logs.size();
+//         vector<string> res;
+//         for(int i=0;i<n;i++){
+//             if(logs[i]=="./");
+//             else if(logs[i]=="../" and res.size()>=1) res.pop_back();
+//             else if(logs[i]=="../");
+//             else res.push_back(logs[i]);
+//         }
         
-        for(int i=0;i<res.size();i++){
-            cout<<res[i]<<" ";
+        
+//         return res.size();
+        
+        
+        stack<string> st;
+        int n=logs.size();
+        int i=0;
+        while(i<n)
+        {
+            if(logs[i]=="./")
+            {
+                i++;
+                continue;
+            }
+            else if((logs[i]=="../")&&(!st.empty()))
+                st.pop();
+            else if((logs[i]=="../")&&(st.empty()))
+            {
+                i++;
+                continue;
+            }
+            else
+                st.push(logs[i]);
+            i++;
         }
-        return res.size();
+        return st.size();
     }
 };
