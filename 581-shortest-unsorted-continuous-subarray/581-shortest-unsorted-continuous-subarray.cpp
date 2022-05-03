08ce7a8324343c01ead131c2cf52b76763e2f6fb
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-05-03 15:08:52 +0530

    Time: 38 ms (66.74%), Space: 26.5 MB (89.65%) - LeetHub

--- 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
+++ 581-shortest-unsorted-continuous-subarray/581-shortest-unsorted-continuous-subarray.cpp
@@ -3,39 +3,38 @@ public:
     int findUnsortedSubarray(vector<int>& nums) {
         int n = nums.size();
         if(n<=1) return 0;
-//         int start=0,end=0;
-//         for(int i=0;i<n-1;i++){
-//             if(nums[i]>nums[i+1]){
-//                 start=i;
-//                 break;
-//             } 
-//         }
-//         for(int i=0;i<n-1;i++){
-//             if(nums[i]<nums[i+1]){
-//                 end=i;
-//             } 
-//         }
-//         if(start==0 and end==0) return n;
-//         end++;
-//         cout<<start<<" "<<end<<endl;
-//         if(start==0 and end==n-1) return 0;
-        
-//         return end-start;
-        
-        
-        
-        
-        
-        vector<int> sortedVec(nums.begin(),nums.end());
-        sort(sortedVec.begin(),sortedVec.end());
-        int start=0,end=n-1;
-        while(start<n and nums[start]==sortedVec[start]){
-            start++;
+        int start=0,end=0,maxm=INT_MIN,minm=INT_MAX;
+        for(int i=0;i<n;i++){
+            maxm = max(maxm,nums[i]);
+            if(maxm>nums[i]){
+                end=i;
+            } 
         }
-        while(end>start and nums[end]==sortedVec[end]){
-            end--;
+        for(int i=n-1;i>=0;i--){
+            minm = min(minm,nums[i]);
+            if(minm<nums[i]){
+                start=i;
+            } 
         }
+        // if(start==0 and end==0) return n;
+        if(start==end) return 0;
         
         return end-start+1;
+        
+        
+        
+        
+        
+//         vector<int> sortedVec(nums.begin(),nums.end());
+//         sort(sortedVec.begin(),sortedVec.end());
+//         int start=0,end=n-1;
+//         while(start<n and nums[start]==sortedVec[start]){
+//             start++;
+//         }
+//         while(end>start and nums[end]==sortedVec[end]){
+//             end--;
+//         }
+        
+//         return end-start+1;
     }
 };
