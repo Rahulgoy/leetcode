f91cd04b46990ec17e414bb1aad459c58fcb89a4
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-05-04 15:08:20 +0530

    Time: 182 ms (59.07%), Space: 58.1 MB (80.67%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     int maxOperations(vector<int>& nums, int k) {
-        unordered_map<int,int> mp;
-        int n= nums.size();
-        for(int i=0;i<n;i++){
-            mp[nums[i]]++;
-        }
-        int count=0;
-        for(int i=0;i<n;i++){
-            if(mp[nums[i]]){
-                int temp = k - nums[i];
-                // cout<<mp[nums[i]]<<" "<<temp<<endl;
-                mp[nums[i]]--;
-                if(mp[temp]){
-                    count++;
-                    mp[temp]--;
-                }else mp[nums[i]]++;
-            }
-            
-        }
         
-        return count;
+        int n= nums.size();
+//      unordered_map<int,int> mp;
+//         for(int i=0;i<n;i++){
+//             mp[nums[i]]++;
+//         }
+//         int count=0;
+//         for(int i=0;i<n;i++){
+//             if(mp[nums[i]]){
+//                 int temp = k - nums[i];
+//                 // cout<<mp[nums[i]]<<" "<<temp<<endl;
+//                 mp[nums[i]]--;
+//                 if(mp[temp]){
+//                     count++;
+//                     mp[temp]--;
+//                 }else mp[nums[i]]++;
+//             }
+            
+//         }
+        
+//         return count;
+        
+        sort(nums.begin(),nums.end());
+        int i=0,j=n-1,ans=0;
+        while(i<j){
+            if(nums[i]+nums[j]==k){
+                ans++;i++;j--;
+            }else if(nums[i]+nums[j]>k) j--;
+            else i++;
+        }
+        return ans;
     }
 };
