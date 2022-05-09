706d168d9c33eaaef2f109df34923353e8727478
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-05-09 14:48:19 +0530

    Time: 2 ms (44.02%), Space: 6.6 MB (60.00%) - LeetHub

--- 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/17-letter-combinations-of-a-phone-number.cpp
@@ -1,53 +1,53 @@
 class Solution {
 public:
     
-//     string helper(){
-        
-//     }
-    
-    vector<string> letterCombinations(string digits) {
-//         map<char,vector<char>> mp;
-//         mp['2']={"a','b','c'};
-//         mp['3']={'d','e','f'};
-//         mp['4']={'g','h','i'};
-//         mp['5']={'j','k','l'};
-//         mp['6']={'m','n','o'};
-//         mp['7']={'p','q','r','s'};
-//         mp['8']={'t','u','v'};
-//         mp['9']={'w','x','y','z'};
-        
-        
-//         int n=digits.size();
-//         if(n==0) return {""};
-//         if(n==1) return mp[digits[0]];
-// //         for(int i=0;i<n;i++){
-            
-// //         }
-        
-        
-//         return {""};
-                 
-                 
-                 
-        if(digits.empty()) 
-            return {};
-        vector<string> map = {"","","abc","def","ghi", "jkl","mno","pqrs","tuv","wxyz"};
-        vector<string> cur(1,"");
-        
-        for(auto digit: digits)
-        {
-            string letters = map[digit-'0'];
-            vector<string> next;
-            for(auto ch : letters)
-                for(auto elem : cur)
-                    next.push_back(elem+ch);
-            swap(cur,next);
+    void solve(string digit,string output,int index,vector<string> &ans,string mapping[]){
+        if(index>=digit.length()){
+            ans.push_back(output);
+            return;
         }
         
-        return cur;  
-                 
-                 
-                 
-                 
+        string value = mapping[digit[index]-'0'];
+        
+        for(int i=0;i<value.length();i++){
+            output.push_back(value[i]);
+            solve(digit,output,index+1,ans,mapping);
+            output.pop_back();
+        }
+    }
+    
+    vector<string> letterCombinations(string digits) {
+//         Method 1 Better and Clean
+//         if(digits.empty()) 
+//             return {};
+//         vector<string> map = {"","","abc","def","ghi", "jkl","mno","pqrs","tuv","wxyz"};
+//         vector<string> cur(1,"");
+        
+//         for(auto digit: digits)
+//         {
+//             string letters = map[digit-'0'];
+//             vector<string> next;
+//             for(auto ch : letters)
+//                 for(auto elem : cur)
+//                     next.push_back(elem+ch);
+//             swap(cur,next);
+//         }
+        
+//         return cur;  
+        
+        
+//         Method 2 BackTracking
+        
+        vector<string> ans;
+        if(digits.length() == 0)
+            return ans;
+        
+        string output = "";
+        int index = 0;
+        string mapping[10] = {"", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+        solve(digits, output, index, ans, mapping);
+        return ans;
+        
+         
     }
 };
