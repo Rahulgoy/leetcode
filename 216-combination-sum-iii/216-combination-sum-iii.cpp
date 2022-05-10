bc15cacde42b29539c24d6167d33dad1c0a32e99
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-05-10 23:41:56 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (58.80%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+    vector<vector<int>> res;
+public:
+    void solve(int i,int k,int n,vector<int>& temp){
+        if(k==0 and n==0){
+            res.push_back(temp);
+            return;
+        }
+        if(k<0 or n<0) return;
+        
+        for(int t=i;t<=9;t++){
+            temp.push_back(t);
+            solve(t+1,k-1,n-t,temp);
+            temp.pop_back();
+        }
+    }
+    vector<vector<int>> combinationSum3(int k, int n) {
+        vector<int>temp;
+        solve(1,k,n,temp);
+        return res;
+    }
+};
