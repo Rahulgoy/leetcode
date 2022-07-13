bd8eeaec3943edb678b279c69fd8120198093d91
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-07-13 13:27:31 +0530

    Time: 6 ms (23.76%), Space: 8.2 MB (90.39%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,13 +1,21 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        int num0=0,num1=0;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i]==0) num0++;
-            else if(nums[i]==1) num1++;
+        // Method 1
+//         int num0=0,num1=0;
+//         for(int i=0;i<nums.size();i++){
+//             if(nums[i]==0) num0++;
+//             else if(nums[i]==1) num1++;
+//         }
+//         for(int i=0;i<num0;i++) nums[i]=0;
+//         for(int i=num0;i<num1+num0;i++) nums[i]=1;
+//         for(int i=num0+num1;i<nums.size();i++) nums[i]=2;
+        
+        // Method 2 
+        int second=nums.size()-1,zero=0;
+        for(int i=0;i<=second;i++){
+            while(nums[i]==2 && i<second) swap(nums[i],nums[second--]);
+            while(nums[i]==0 && i>zero) swap(nums[i],nums[zero++]);
         }
-        for(int i=0;i<num0;i++) nums[i]=0;
-        for(int i=num0;i<num1+num0;i++) nums[i]=1;
-        for(int i=num0+num1;i<nums.size();i++) nums[i]=2;
     }
 };
