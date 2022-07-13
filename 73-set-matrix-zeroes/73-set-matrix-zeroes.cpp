872f47b74d6ee1b412e40947f029854fef45fe3b
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-07-13 11:44:22 +0530

    Time: 25 ms (39.65%), Space: 13.2 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -1,3 +1,4 @@
+#include <bits/stdc++.h>
 class Solution {
 public:
     void setZ(vector<vector<int>>& matrix,int i,int j){
@@ -11,16 +12,45 @@ public:
             matrix[i][a]=0;
         }
     }
+    
+    
     void setZeroes(vector<vector<int>>& matrix) {
-        vector<vector<int>> res(matrix.begin(),matrix.end());
+        // Method 1 without inplace         
+//         vector<vector<int>> res(matrix.begin(),matrix.end());
+//         for(int i=0;i<matrix.size();i++){
+//             for(int j=0;j<matrix[i].size();j++){
+//                 if(matrix[i][j]==0){
+//                     setZ(res,i,j);
+//                 }
+//             }
+//         }
+        
+//         matrix=res;
+        
+        // Method 2 with inplace
+        bool row=false,col=false;
         for(int i=0;i<matrix.size();i++){
             for(int j=0;j<matrix[i].size();j++){
                 if(matrix[i][j]==0){
-                    setZ(res,i,j);
+                    if(i==0) row=true;
+                    if(j==0) col=true;
+                    matrix[0][j]=matrix[i][0]=0;  // to set starting elem of that row and column to zero
+                    
                 }
             }
         }
         
-        matrix=res;
+        for(int i=1;i<matrix.size();i++){
+            for(int j=1;j<matrix[i].size();j++){
+                if(matrix[i][0]==0||matrix[0][j]==0) matrix[i][j]=0;
+            }
+        }
+        if(col){
+            for(int i=0;i<matrix.size();i++) matrix[i][0]=0;
+        }
+        if(row){
+            for(int i=0;i<matrix[0].size();i++) matrix[0][i]=0;
+        }
+        
     }
 };
