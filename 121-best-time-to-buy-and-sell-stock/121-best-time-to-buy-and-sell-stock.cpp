743386bd0fa2186d5af4f56dbe4cec38a59683f3
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-07-13 13:39:28 +0530

    Time: 194 ms (52.59%), Space: 93.4 MB (52.30%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int profit=0;
-        int maxi=prices[0],mini=prices[0];
-        for(int i=0;i<prices.size();i++){
-            if(prices[i]<mini){
-                mini=maxi=prices[i];
-            }else if(prices[i]>mini and prices[i]>maxi){
-                maxi=prices[i];
-            }
-            profit=max(profit, maxi-mini);
-        }
-        return profit;
-        
-        
-        //   Iterating from Backwards      
-        // int n = prices.size();
-        // int mx = prices[n - 1];
-        // int mx_profit = 0, price = 0;
-        // // in a reversive order, make sure max is behind now
-        // for (int i = n - 1; i >= 0; i--){
-        //     price = prices[i];
-        //     mx = mx > price? mx:price;
-        //     mx_profit = mx - price > mx_profit? mx - price : mx_profit;
+        // int maxi=prices[0],mini=prices[0],max_profit=0;
+        // for(int i=0;i<prices.size();i++){
+        //     if(prices[i]<mini) mini=maxi=prices[i];
+        //     else if(prices[i]>mini and prices[i]>maxi) maxi=prices[i];
+        //     max_profit= max(max_profit,maxi-mini);
         // }
-        // return mx_profit;
+        // return max_profit;
+        
+        
+        int mx=prices[prices.size()-1],max_price=0,price;
+        
+        for(int i=prices.size()-1;i>=0;i--){
+            price=prices[i];
+            mx=max(mx,price);
+            max_price=max(max_price,mx-price);
+        }
+        return max_price;
     }
 };
