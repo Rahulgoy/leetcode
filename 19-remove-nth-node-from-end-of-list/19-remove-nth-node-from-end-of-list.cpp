ee4220a4920b3dc7e7bcda9d77c9d2a10562c826
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-07-28 20:09:05 +0530

    Time: 10 ms (30.21%), Space: 10.8 MB (31.65%) - LeetHub

--- 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/19-remove-nth-node-from-end-of-list.cpp
@@ -11,27 +11,42 @@
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        if(!head->next and n==1) return NULL;
         
+        //  Method 1 - Two passes        
+//         if(!head->next and n==1) return NULL;
+            
+//         ListNode* temp=head;
+//         int count=1;
+//         while(temp->next!=NULL){
+//             temp=temp->next;
+//             count++;
+//         }
+//         count=count-n-1;
+//         if(count<0){
+//             head=head->next;
+//             return head;
+//         } 
+//         temp=head;
+//         while(count>0){
+//             temp=temp->next;
+//             count--;
+//         }
+//         if(n==1) temp->next=NULL;
+//         else temp->next=temp->next->next;
+//         return head;
         
-        ListNode* temp=head;
-        int count=1;
-        while(temp->next!=NULL){
-            temp=temp->next;
-            count++;
+        // Method 2 One pass
+        ListNode * start = new ListNode();
+        start -> next = head;
+        ListNode* fast = start;
+        ListNode* slow = start;
+        for(int i=1;i<=n;i++) fast=fast->next;
+        while(fast->next!=NULL){
+            fast=fast->next;
+            slow=slow->next;
         }
-        count=count-n-1;
-        if(count<0){
-            head=head->next;
-            return head;
-        } 
-        temp=head;
-        while(count>0){
-            temp=temp->next;
-            count--;
-        }
-        if(n==1) temp->next=NULL;
-        else temp->next=temp->next->next;
-        return head;
+        if(!slow->next->next) slow->next=NULL;
+        else slow->next=slow->next->next;
+        return start->next;
     }
 };
