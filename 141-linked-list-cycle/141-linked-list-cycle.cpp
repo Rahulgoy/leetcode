f603bb3d09a92c2409e7a31e2dcd3d329dd682ed
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-07-30 23:28:27 +0530

    Time: 18 ms (45.21%), Space: 8 MB (57.63%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,14 +9,28 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        unordered_map<ListNode*, int> mp;
-        ListNode* temp=head;
-        while(temp){
-            if(mp[temp]) return true;
-            else mp[temp]++;
-            temp=temp->next;
+        // Method 1 Hashing
+        // unordered_map<ListNode *,int> mp;
+        // ListNode *temp = head;
+        // while(temp!=NULL){
+        //     if(mp[temp]) return true;
+        //     else mp[temp]++;
+        //     temp=temp->next;
+        // }
+        // return false;
+        
+        // Method 2 Fast and Slow pointer
+        if(!head) return head;
+        ListNode *slow=head;
+        ListNode *fast=head;
+        
+        while(fast->next and fast->next->next){
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast) return true;
             
         }
         return false;
+        
     }
 };
