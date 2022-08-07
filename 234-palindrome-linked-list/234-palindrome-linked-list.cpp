0eabb9e9734f5c0aa303bbfe04265ccdd324d4dd
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-08-07 17:57:09 +0530

    Time: 345 ms (58.74%), Space: 117.9 MB (62.54%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -10,17 +10,47 @@
  */
 class Solution {
 public:
+    ListNode *reverse(ListNode* head){
+        ListNode *prevNode=NULL,*currNode=head,*nextNode=head;
+        while(currNode){
+            nextNode=currNode->next;
+            currNode->next=prevNode;
+            prevNode=currNode;
+            currNode=nextNode;
+            
+        }
+        return prevNode;
+    }
     bool isPalindrome(ListNode* head) {
         // Method 1
-        vector<int> clone;
-        ListNode* temp=head;
-        while(temp){
-            clone.push_back(temp->val);
-            temp=temp->next;
+        // vector<int> clone;
+        // ListNode* temp=head;
+        // while(temp){
+        //     clone.push_back(temp->val);
+        //     temp=temp->next;
+        // }
+        // for(int i=0;i<clone.size()/2;i++){
+        //     if(clone[i]!=clone[clone.size()-i-1]) return false;
+        // }
+        // return true;
+        
+        //Method 2
+        if(!head or !head->next) return head;
+        ListNode *slow=head,*fast=head;
+        while(fast->next and fast->next->next){
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        for(int i=0;i<clone.size()/2;i++){
-            if(clone[i]!=clone[clone.size()-i-1]) return false;
+        
+        slow->next = reverse(slow->next);
+        fast=slow->next;
+        slow=head;
+        while(fast){
+            if(slow->val!=fast->val) return false;
+            slow=slow->next;
+            fast=fast->next;
         }
         return true;
     }
+    
 };
