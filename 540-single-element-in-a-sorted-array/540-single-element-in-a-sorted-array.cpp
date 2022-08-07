25cf8b3368b91e2f78a59d83b0d601b053fc9b86
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-08-08 00:18:47 +0530

    Time: 54 ms (21.86%), Space: 22.4 MB (55.64%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,10 +1,22 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int res=nums[0];
-        for(int i=1;i<nums.size();i++){
-            res=res^nums[i];
+        // Method 1 Using Bit Manipulation
+        // int res=nums[0];
+        // for(int i=1;i<nums.size();i++){
+        //     res=res^nums[i];
+        // }
+        // return res;
+        
+        // Method 2 Binary Search
+        int low=0,high=nums.size()-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+            if((mid%2==0 and nums[mid]==nums[mid+1]) ||(mid%2==1 and nums[mid]==nums[mid-1])){
+                low=mid+1;
+            }else high=mid;
+            
         }
-        return res;
+        return nums[low];
     }
 };
