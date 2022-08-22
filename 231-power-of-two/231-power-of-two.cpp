da014804046b81e0693350ed1c28a9ff78f53f16
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-08-22 21:44:07 +0530

    Time: 6 ms (21.06%), Space: 6 MB (26.88%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isPowerOfTwo(int num) {
-        return num > 0 && (num & (num - 1)) == 0;
+        // return num > 0 && (num & (num - 1)) == 0;
+        
+        return fmod(log10(num)/log10(2), 1)==0;
     }
 };
