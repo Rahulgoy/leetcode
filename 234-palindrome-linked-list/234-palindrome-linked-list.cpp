cf629e1f7e6b438a50e5733205d1fa0dcf827cfc
Rahul Goyal <54597682+Rahulgoy@users.noreply.github.com>
2022-08-23 12:39:27 +0530

    Time: 309 ms (70.03%), Space: 114.2 MB (74.71%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -11,39 +11,25 @@
 class Solution {
 public:
     ListNode *reverse(ListNode* head){
-        ListNode *prevNode=NULL,*currNode=head,*nextNode=head;
+        ListNode *currNode=head,*prevNode=NULL,*nextNode=head;
+        
         while(currNode){
             nextNode=currNode->next;
             currNode->next=prevNode;
             prevNode=currNode;
             currNode=nextNode;
-            
         }
         return prevNode;
     }
     bool isPalindrome(ListNode* head) {
-        // Method 1
-        // vector<int> clone;
-        // ListNode* temp=head;
-        // while(temp){
-        //     clone.push_back(temp->val);
-        //     temp=temp->next;
-        // }
-        // for(int i=0;i<clone.size()/2;i++){
-        //     if(clone[i]!=clone[clone.size()-i-1]) return false;
-        // }
-        // return true;
-        
-        //Method 2
-        if(!head or !head->next) return head;
         ListNode *slow=head,*fast=head;
-        while(fast->next and fast->next->next){
+        
+        while(fast->next!=NULL && fast->next->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
         }
         
-        slow->next = reverse(slow->next);
-        fast=slow->next;
+        fast = reverse(slow->next);
         slow=head;
         while(fast){
             if(slow->val!=fast->val) return false;
